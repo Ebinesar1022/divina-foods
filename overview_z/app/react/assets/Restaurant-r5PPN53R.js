d977899f62b0f9eaa0037ce81d9af2873c9d78a1
Lourthu Xavier M <xavierstayfoolish@gmail.com>
2026-09-22 19:52:32 +0530

    new changes added

--- overview_z/app/react/assets/Restaurant-r5PPN53R.js
+++
@@ -1,1 +0,0 @@
-import{r,j as t,i as a}from"./index-B9B30YLF.js";var e={},u=a;Object.defineProperty(e,"__esModule",{value:!0});var v=e.default=void 0,o=u(r()),s=t;v=e.default=(0,o.default)((0,s.jsx)("path",{d:"M11 9H9V2H7v7H5V2H3v7c0 2.12 1.66 3.84 3.75 3.97V22h2.5v-9.03C11.34 12.84 13 11.12 13 9V2h-2zm5-3v8h2.5v8H21V2c-2.76 0-5 2.24-5 4"}),"Restaurant");export{v as d};
--- overview_z/app/react/assets/Warehouse-Bw3B_oDy.js
+++
@@ -1,1 +0,0 @@
-import{r,j as a,i as t}from"./index-B9B30YLF.js";var e={},u=t;Object.defineProperty(e,"__esModule",{value:!0});var v=e.default=void 0,o=u(r()),s=a;v=e.default=(0,o.default)((0,s.jsx)("path",{d:"M22 21V7L12 3 2 7v14h5v-9h10v9zm-11-2H9v2h2zm2-3h-2v2h2zm2 3h-2v2h2z"}),"Warehouse");export{v as d};
